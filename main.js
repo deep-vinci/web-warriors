00dfe59d3169462ebd53331d34e2488033320bcd
Deepak <deepakworkpc@gmail.com>
2025-02-13 22:37:07 +0530

    feat: add responsive nav

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("test");
+// const r = new rive.Rive({
+//     src: "https://cdn.rive.app/animations/vehicles.riv",
+//     // OR the path to a discoverable and public Rive asset
+//     // src: '/public/example.riv',
+//     canvas: document.getElementById("canvas"),
+//     autoplay: true,
+//     // artboard: "Arboard", // Optional. If not supplied the default is selected
+//     stateMachines: "bumpy",
+//     onLoad: () => {
+//         r.resizeDrawingSurfaceToCanvas();
+//     },
+// });
