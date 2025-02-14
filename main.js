8bbea05079fc08c46823695d3f44b8ebaaa51e42
Deepak <deepakworkpc@gmail.com>
2025-02-14 12:23:30 +0530

    push3

--- main.js
+++ main.js
@@ -11,14 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
     // });
 });
 
-// Select all question buttons
 const faqQuestions = document.querySelectorAll(".faq-question");
 
-// Loop through each question button
 faqQuestions.forEach((question) => {
-    // Add a click event listener to each question
     question.addEventListener("click", () => {
-        // Close any other open answers except the one clicked
         faqQuestions.forEach((item) => {
             if (item !== question) {
                 item.classList.remove("active"); // Remove 'active' class to reset arrow rotation
@@ -26,18 +22,13 @@ faqQuestions.forEach((question) => {
             }
         });
 
-        // Toggle 'active' class on the clicked question to rotate the arrow
         question.classList.toggle("active");
 
-        // Select the corresponding answer div
         const answer = question.nextElementSibling;
 
-        // Check if the answer is already open
         if (answer.style.maxHeight) {
-            // If open, close it by resetting max-height
             answer.style.maxHeight = null;
         } else {
-            // If closed, set max-height to scrollHeight to expand it
             answer.style.maxHeight = answer.scrollHeight + "px";
         }
     });
